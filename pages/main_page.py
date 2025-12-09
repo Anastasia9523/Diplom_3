042944b4b5612f7b9827877e7de8a2458ed40f62
Anastasia Iliasova <anastasia.rugby@yandex.ru>
2025-12-09 23:15:15 +0300

    Удалены вызовы ModalPage внутри MainPage, убраны фиксированные ожидания

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,9 @@
 import allure
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 
-from locators.modal_locators import ModalLocators
 from pages.base_page import BasePage
-from pages.modal_page import ModalPage
 from locators.main_page_locators import MainPageLocators as L
 from data.urls import BASE_URL
 
@@ -19,25 +16,15 @@ class MainPage(BasePage):
 
     @allure.step("Перейти в Конструктор")
     def go_to_constructor(self):
-        modal = ModalPage(self.driver)
-        modal.close_if_open()
-
-        WebDriverWait(self.driver, 3).until(EC.invisibility_of_element_located(ModalLocators.CONTAINER))
-
-        self.click(L.CONSTRUCTOR_BTN)
+        self.wait.until(EC.element_to_be_clickable(L.CONSTRUCTOR_BTN)).click()
 
     @allure.step("Перейти в Ленту заказов")
     def go_to_feed(self):
-        modal = ModalPage(self.driver)
-        modal.close_if_open()
-
-        WebDriverWait(self.driver, 3).until(EC.invisibility_of_element_located(ModalLocators.CONTAINER))
-
-        self.click(L.FEED_BTN)
+        self.wait.until(EC.element_to_be_clickable(L.FEED_BTN)).click()
 
     @allure.step("Ожидание загрузки конструктора")
     def wait_constructor_loaded(self):
-        self.find(L.CONSTRUCTOR_DROP_ZONE)
+        self.wait.until(EC.visibility_of_element_located(L.CONSTRUCTOR_DROP_ZONE))
 
     @allure.step("Получить список ингредиентов")
     def get_all_ingredients(self):
@@ -45,43 +32,36 @@ class MainPage(BasePage):
 
     @allure.step("Клик по ингредиенту №{index}")
     def click_ingredient(self, index):
-        items = self.get_all_ingredients()
-        items[index].click()
+        self.get_all_ingredients()[index].click()
 
     @allure.step("Получить счётчик ингредиента №{index}")
     def get_ingredient_counter(self, index):
-        items = self.get_all_ingredients()
-        ingredient = items[index]
-
+        ingredient = self.get_all_ingredients()[index]
         blocks = ingredient.find_elements(*L.INGREDIENT_COUNTER)
         if not blocks:
             return 0
-
         try:
-            p = blocks[0].find_element(By.TAG_NAME, "p")
-            return int(p.text.strip())
+            return int(blocks[0].find_element(By.TAG_NAME, "p").text.strip())
         except:
             return 0
 
     @allure.step("Перетаскивание ингредиента №{index}")
     def drag_ingredient_to_constructor_by_index(self, index):
 
-        ingredients = self.get_all_ingredients()
-        source = ingredients[index]
-
+        source = self.get_all_ingredients()[index]
         target = self.find(L.CONSTRUCTOR_DROP_ZONE)
+
+        self.wait.until(EC.visibility_of(source))
+        self.wait.until(EC.visibility_of_element_located(L.CONSTRUCTOR_DROP_ZONE))
+        self.wait.until(EC.element_to_be_clickable(L.CONSTRUCTOR_DROP_ZONE))
 
         self.driver.execute_script("arguments[0].scrollIntoView({block:'center'});", source)
         self.driver.execute_script("arguments[0].scrollIntoView({block:'center'});", target)
 
-        actions = ActionChains(self.driver)
+        ActionChains(self.driver).click_and_hold(source).move_to_element(target).release().perform()
 
-        actions.move_to_element(source).pause(0.2)
-        actions.click_and_hold(source).pause(0.3)
-        actions.move_to_element(target).pause(0.3)
-        actions.move_by_offset(0, 40).pause(0.3) 
-        actions.release().perform()
-   
+        self.wait.until(lambda d: self.get_ingredient_counter(index) > 0)
+
     @allure.step("Нажать кнопку 'Оформить заказ'")
     def click_order_button(self):
         self.click(L.ORDER_BUTTON)
@@ -99,4 +79,4 @@ class MainPage(BasePage):
                 self.drag_ingredient_to_constructor_by_index(i)
                 return
             except:
-                pass
+                continue
