229598935900ad2e49197f560c063c1f32d2065c
Anastasia Iliasova <anastasia.rugby@yandex.ru>
2025-12-10 22:56:16 +0300

    Добавлен метод scroll_into_view(), добавлен метод drag_and_drop(), PageObject-классы очищены от прямых обращений к driver

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 import allure
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 
 class BasePage:
 
@@ -20,3 +21,29 @@ class BasePage:
     def find_all(self, locator):
         self.wait.until(EC.presence_of_all_elements_located(locator))
         return self.driver.find_elements(*locator)
+
+    @allure.step("Скролл к элементу")
+    def scroll_into_view(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView({block:'center'});", element)
+
+    @allure.step("Перемещение элемента drag&drop")
+    def drag_and_drop(self, source, target):
+        action = ActionChains(self.driver)
+        action.drag_and_drop(source, target).perform()
+
+    def wait_modal_closed(self):
+        from locators.modal_locators import ModalLocators as ML
+        try:
+            self.short_wait().until(EC.invisibility_of_element_located(ML.CONTAINER))
+        except:
+            pass
+        try:
+            self.short_wait().until(EC.invisibility_of_element_located(ML.OVERLAY))
+        except:
+            pass
+
+    def js_click(self, element):
+        self.driver.execute_script("arguments[0].click();", element)
+
+    def short_wait(self, timeout=0.5):
+        return WebDriverWait(self.driver, timeout)
